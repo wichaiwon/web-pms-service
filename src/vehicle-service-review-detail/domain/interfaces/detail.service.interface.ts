3ad18c3ed6d9d39ec194583872541e352f616ab1
wichaiwon <wichai.wongfu.pkg@gmail.com>
2025-12-30 09:52:57 +0700

    add interface get detail,additional to repo,service. interface

--- src/vehicle-service-review-detail/domain/interfaces/detail.service.interface.ts
+++ src/vehicle-service-review-detail/domain/interfaces/detail.service.interface.ts
@@ -7,11 +7,17 @@ import { UpdateDetailAdditionalDto } from "src/vehicle-service-review-detail/int
 import { UpdateDetailDto } from "src/vehicle-service-review-detail/interfaces/dtos/update-detail.dto";
 
 export interface IDetailServiceInterface {
+    getDetailById(id: string): Promise<DetailDto | null>;
+    getDetailByReviewId(vehicleServiceReviewId: string): Promise<DetailDto | null>;
     createDetail(createDto: CreateDetailDto): Promise<DetailDto>;
     createDetails(createDto: CreateDetailDto[]): Promise<DetailDto[]>;
     updateDetail(id: string, updateDto: UpdateDetailDto): Promise<DetailDto>;
     patchSuccessFlag(id: string, patchDto: PatchDetailDto): Promise<DetailDto>;
     patchIsActive(id: string, patchDto: PatchDetailDto): Promise<DetailDto>;
+    getDetailAdditionalById(id: string): Promise<DetailAdditionalDto | null>;
+    getDetailAdditionalByDetailId(detailId: string): Promise<DetailAdditionalDto | null>;
     createDetailAdditional(createDto: CreateDetailAdditionalDto): Promise<DetailAdditionalDto>;
     updateDetailAdditional(id: string, updateDto: UpdateDetailAdditionalDto): Promise<DetailAdditionalDto>;
+    patchDetailAdditionalIsActive(id: string, patchDto: PatchDetailDto): Promise<DetailAdditionalDto>;
+    patchDetailAdditionalSuccessFlag(id: string, patchDto: PatchDetailDto): Promise<DetailAdditionalDto>;
 }
