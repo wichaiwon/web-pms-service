334cabb0f039ddf3efc7bee57b1921620f48914d
wichaiwon <wichai.wongfu.pkg@gmail.com>
2025-12-30 15:32:56 +0700

    adjust value to null

--- src/vehicle-service-review-step-two/interfaces/dtos/update-step-two-additional.dto.ts
+++ src/vehicle-service-review-step-two/interfaces/dtos/update-step-two-additional.dto.ts
@@ -5,23 +5,21 @@ export class UpdateStepTwoAdditionalDto {
     
     @ApiProperty({ isArray: true, type: String, example: ['https://image4.com'] })
     @IsOptional()
-    left_front_tire_image: string[];
+    left_front_tire_image?: string[];
 
     @ApiProperty({ isArray: true, type: String, example: ['https://image3.com'] })
     @IsOptional()
-    right_front_tire_image: string[];
-
+    right_front_tire_image?: string[];
     @ApiProperty({ isArray: true, type: String, example: ['https://image2.com'] })
     @IsOptional()
-    left_back_tire_image: string[];
+    left_back_tire_image?: string[];
 
     @ApiProperty({ isArray: true, type: String, example: ['https://image1.com'] })
     @IsOptional()
-    right_back_tire_image: string[];
-
+    right_back_tire_image?: string[];
     @ApiProperty({ example: 'ยางสภาพดี' })
     @IsOptional()
-    comment: string;
+    comment?: string;
 
     @ApiProperty({ example: '550e8400-e29b-41d4-a716-446655440000' })
     @IsNotEmpty()
--- src/vehicle-service-review-step-two/interfaces/dtos/update-step-two.dto.ts
+++ src/vehicle-service-review-step-two/interfaces/dtos/update-step-two.dto.ts
@@ -5,83 +5,74 @@ import { Cargo, SpareTire, TireCondition, TireDepth, TruckToolSet, WheelControlC
 export class UpdateStepTwoDto {
     @ApiProperty({ enum: SpareTire, example: SpareTire.HAVE })
     @IsOptional()
-    spare_tire: SpareTire;
+    spare_tire?: SpareTire;
 
     @ApiProperty({ enum: WheelControlCover, example: WheelControlCover.HAVE })
     @IsOptional()
-    wheel_control_cover: WheelControlCover;
-
+    wheel_control_cover?: WheelControlCover;
     @ApiProperty({ enum: Cargo, example: Cargo.HAVE })
     @IsOptional()
-    cargo: Cargo;
+    cargo?: Cargo;
 
     @ApiProperty({ enum: TruckToolSet, example: TruckToolSet.HAVE })
     @IsOptional()
-    truck_tool_set: TruckToolSet;
-
+    truck_tool_set?: TruckToolSet;
     @ApiProperty({ example: '25', description: 'ปีของยางล้อหน้าซ้าย' })
     @IsOptional()
-    left_front_tire_year: string;
+    left_front_tire_year?: string;
 
     @ApiProperty({ example: '25', description: 'ปีของยางล้อหน้าขวา' })
     @IsOptional()
-    right_front_tire_year: string;
-
+    right_front_tire_year?: string;
     @ApiProperty({ example: '25', description: 'ปีของยางล้อหลังซ้าย' })
     @IsOptional()
-    left_back_tire_year: string;
+    left_back_tire_year?: string;
 
     @ApiProperty({ example: '25', description: 'ปีของยางล้อหลังขวา' })
     @IsOptional()
-    right_back_tire_year: string;
-
+    right_back_tire_year?: string;
     @ApiProperty({ example: 32, description: 'ความดันลมยางล้อหน้าซ้าย' })
     @IsOptional()
-    left_front_tire_pressure: number;
+    left_front_tire_pressure?: number;
 
     @ApiProperty({ example: 32, description: 'ความดันลมยางล้อหน้าขวา' })
     @IsOptional()
-    right_front_tire_pressure: number;
-
+    right_front_tire_pressure?: number;
     @ApiProperty({ example: 32, description: 'ความดันลมยางล้อหลังซ้าย' })
     @IsOptional()
-    left_back_tire_pressure: number;
+    left_back_tire_pressure?: number;
 
     @ApiProperty({ example: 32, description: 'ความดันลมยางล้อหลังขวา' })
     @IsOptional()
-    right_back_tire_pressure: number;
-
+    right_back_tire_pressure?: number;
     @ApiProperty({ enum:TireDepth, example: TireDepth.EIGHT, description: 'ความลึกของดอกยางล้อหน้าซ้าย' })
     @IsOptional()
-    left_front_tire_depth: TireDepth;
+    left_front_tire_depth?: TireDepth;
 
     @ApiProperty({ enum:TireDepth, example: TireDepth.EIGHT, description: 'ความลึกของดอกยางล้อหน้าขวา' })
     @IsOptional()
-    right_front_tire_depth: TireDepth;
-
+    right_front_tire_depth?: TireDepth;
     @ApiProperty({ enum:TireDepth, example: TireDepth.EIGHT, description: 'ความลึกของดอกยางล้อหลังซ้าย' })
     @IsOptional()
-    left_back_tire_depth: TireDepth;
+    left_back_tire_depth?: TireDepth;
 
     @ApiProperty({ enum:TireDepth, example: TireDepth.EIGHT, description: 'ความลึกของดอกยางล้อหลังขวา' })
     @IsOptional()
-    right_back_tire_depth: TireDepth;
-
+    right_back_tire_depth?: TireDepth;
     @ApiProperty({enum:TireCondition, example: TireCondition.CHANGE_IMMEDIATELY, description: 'สภาพยางล้อหน้าซ้าย' })
     @IsOptional()
-    left_front_tire_condition: TireCondition;
+    left_front_tire_condition?: TireCondition;
 
     @ApiProperty({enum:TireCondition, example: TireCondition.CHANGE_IMMEDIATELY, description: 'สภาพยางล้อหน้าขวา' })
     @IsOptional()
-    right_front_tire_condition: TireCondition;
-
+    right_front_tire_condition?: TireCondition;
     @ApiProperty({enum:TireCondition, example: TireCondition.CHANGE_IMMEDIATELY, description: 'สภาพยางล้อหลังซ้าย' })
     @IsOptional()
-    left_back_tire_condition: TireCondition;
+    left_back_tire_condition?: TireCondition;
 
     @ApiProperty({enum:TireCondition, example: TireCondition.CHANGE_IMMEDIATELY, description: 'สภาพยางล้อหลังขวา' })
     @IsOptional()
-    right_back_tire_condition: TireCondition;
+    right_back_tire_condition?: TireCondition;
     
     @ApiProperty({ example: '550e8400-e29b-41d4-a716-446655440000' })
     @IsNotEmpty()
