e9b32ee9b8e1036fbe1d92df84ba66d39aab2bf1
wichaiwon <wichai.wongfu.pkg@gmail.com>
2025-12-30 09:52:22 +0700

    add validation for get detail by reviewId

--- src/vehicle-service-review-detail/application/queries/get-detail-by-review-id.use-case.ts
+++ src/vehicle-service-review-detail/application/queries/get-detail-by-review-id.use-case.ts
@@ -2,15 +2,18 @@ import { Inject, Injectable } from "@nestjs/common";
 import type { IDetailRepositoryInterface } from "src/vehicle-service-review-detail/domain/interfaces/detail.repository.interface";
 import { DetailDto } from "src/vehicle-service-review-detail/interfaces/dtos/detail.dto";
 
-
 @Injectable()
-export class GetDetailByIdUseCase {
+export class GetDetailByReviewIdUseCase {
     constructor(
         @Inject('IDetailRepository')
         private readonly detailRepository: IDetailRepositoryInterface
     ) { }
 
-    async execute(vehicleServiceReviewId: string): Promise<DetailDto | null> {
-        return this.detailRepository.getDetailByReviewId(vehicleServiceReviewId);
+    async execute(ReviewId: string): Promise<DetailDto | null> {
+        const detail = await this.detailRepository.getDetailByReviewId(ReviewId);
+        if (!detail) {
+            throw new Error(`Detail with Review ID ${ReviewId} not found.`);
+        }
+        return detail;
     }
 }
