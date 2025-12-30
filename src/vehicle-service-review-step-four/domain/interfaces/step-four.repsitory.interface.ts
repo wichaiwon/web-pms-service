7dcbd72047a8a57978f336331435c4a82fa9480b
wichaiwon <wichai.wongfu.pkg@gmail.com>
2025-12-30 19:10:09 +0700

    add interface get,bulkinsert to service,repo

--- src/vehicle-service-review-step-four/domain/interfaces/step-four.repsitory.interface.ts
+++ src/vehicle-service-review-step-four/domain/interfaces/step-four.repsitory.interface.ts
@@ -4,9 +4,11 @@ import { UpdateStepfourDto } from "src/vehicle-service-review-step-four/interfac
 import { PatchStepfourDto } from "src/vehicle-service-review-step-four/interfaces/dtos/patch-step-four.dto";
 
 export interface IStepFourRepositoryInterface {
+    getStepFourById(id: string): Promise<VehicleServiceReviewStepFour | null>;
     getStepFourByReviewId(reviewId: string): Promise<VehicleServiceReviewStepFour | null>;
     createStepFour(createDto: CreateStepfourDto): Promise<VehicleServiceReviewStepFour>;
     updateStepFour(id: string, updateDto: UpdateStepfourDto): Promise<VehicleServiceReviewStepFour>;
+    createStepFours(createDtos: CreateStepfourDto[]): Promise<VehicleServiceReviewStepFour[]>;
     patchStepFourIsActive(id: string, patchDto: PatchStepfourDto): Promise<VehicleServiceReviewStepFour>;
     patchStepFourSuccessFlag(id: string, patchDto: PatchStepfourDto): Promise<VehicleServiceReviewStepFour>;
 }
