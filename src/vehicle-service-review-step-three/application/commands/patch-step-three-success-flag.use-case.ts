3c5a44370cf72d8a4832cf25830b79b69de023c6
wichaiwon <wichai.wongfu.pkg@gmail.com>
2025-12-30 16:42:29 +0700

    add validation

--- src/vehicle-service-review-step-three/application/commands/patch-step-three-success-flag.use-case.ts
+++ src/vehicle-service-review-step-three/application/commands/patch-step-three-success-flag.use-case.ts
@@ -1,15 +1,21 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, NotFoundException } from "@nestjs/common";
 import type { IStepThreeRepositoryInterface } from "src/vehicle-service-review-step-three/domain/interfaces/step-three.repository.interface";
 import { PatchStepThreeDto } from "src/vehicle-service-review-step-three/interfaces/dtos/patch-step-three.dto";
 import { StepThreeDto } from "src/vehicle-service-review-step-three/interfaces/dtos/step-three.dto";
 
 @Injectable()
-export class PatchStepthreeSuccessFlagUseCase {
+export class PatchStepThreeSuccessFlagUseCase {
     constructor(
         @Inject('IStepThreeRepository')
         private readonly stepThreeRepository: IStepThreeRepositoryInterface
     ) { }
     async execute(id: string, patchDto: PatchStepThreeDto): Promise<StepThreeDto> {
-        return await this.stepThreeRepository.patchStepThreeSuccessFlag(id, patchDto);
+        const existingStepThree = await this.stepThreeRepository.getStepThreeById(id);
+        if (!existingStepThree) {
+            throw new NotFoundException('Step Three not found');
+        }
+        const updatedIsActive ={ ...patchDto, success_flag: !existingStepThree.success_flag };
+        const updatedStepThree = await this.stepThreeRepository.patchStepThreeSuccessFlag(id, updatedIsActive);
+        return updatedStepThree;
     }
 }
