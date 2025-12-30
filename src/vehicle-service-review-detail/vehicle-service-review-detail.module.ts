c0d0a9602c02767b70510f5c691b0e4dea8a4660
wichaiwon <wichai.wongfu.pkg@gmail.com>
2025-12-30 09:50:07 +0700

    add get modul detail,additional

--- src/vehicle-service-review-detail/vehicle-service-review-detail.module.ts
+++ src/vehicle-service-review-detail/vehicle-service-review-detail.module.ts
@@ -1,20 +1,23 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { VehicleServiceReviewDetail } from "./domain/entities/vehicle-service-review-detail.entity";
+import { VehicleServiceReviewDetailAdditional } from "./domain/entities/vehicle-service-review-detail-additional.entity";
 import { DetailController } from "./interfaces/controllers/detail.controller";
-import { DetailService } from "./application/detail.service";
-import { DetailRepository } from "./infrastructure/repositories/detail.repository";
-import { GetDetailByIdUseCase } from "./application/queries/get-detail-by-review-id.use-case";
+import { GetDetailByReviewIdUseCase } from "./application/queries/get-detail-by-review-id.use-case";
+import { GetDetailByIdUseCase } from "./application/queries/get-detail-by-id.use-case";
 import { CreateDetailUseCase } from "./application/commands/create-detail.use-case";
 import { CreateDetailsUseCase } from "./application/commands/create-details.use-case";
 import { UpdateDetailUseCase } from "./application/commands/update-detail.use-case";
-import { PatchIsActiveUseCase } from "./application/commands/patch-detail-is-active.use-case";
 import { PatchSuccessFlagUseCase } from "./application/commands/patch-detail-success-flag.use-case";
-import { VehicleServiceReviewDetail } from "./domain/entities/vehicle-service-review-detail.entity";
-import { VehicleServiceReviewDetailAdditional } from "./domain/entities/vehicle-service-review-detail-additional.entity";
+import { PatchIsActiveUseCase } from "./application/commands/patch-detail-is-active.use-case";
+import { GetDetailAdditionalByIdUseCase } from "./application/queries/get-additional-by-id.use-case";
+import { GetDetailAdditionalByDetailIdUseCase } from "./application/queries/get-additional-by-detail-id.use-case";
 import { CreateDetailAdditionalUseCase } from "./application/commands/create-detail-additional.use-case";
 import { UpdateDetailAdditionalUseCase } from "./application/commands/update-detail-additional.use-case";
 import { PatchDetailAdditionalSuccessFlagUseCase } from "./application/commands/patch-detail-additional-success-flag.use-case";
 import { PatchDetailAdditionalIsActiveUseCase } from "./application/commands/patch-detail-additional-is-active.use-case";
+import { DetailService } from "./application/detail.service";
+import { DetailRepository } from "./infrastructure/repositories/detail.repository";
 
 @Module({
     imports: [
@@ -23,16 +26,19 @@ import { PatchDetailAdditionalIsActiveUseCase } from "./application/commands/pat
     controllers: [DetailController],
     providers: [
         GetDetailByIdUseCase,
+        GetDetailByReviewIdUseCase,
         CreateDetailUseCase,
         CreateDetailsUseCase,
         UpdateDetailUseCase,
         PatchSuccessFlagUseCase,
         PatchIsActiveUseCase,
-        DetailService,
+        GetDetailAdditionalByIdUseCase,
+        GetDetailAdditionalByDetailIdUseCase,
         CreateDetailAdditionalUseCase,
         UpdateDetailAdditionalUseCase,
         PatchDetailAdditionalSuccessFlagUseCase,
         PatchDetailAdditionalIsActiveUseCase,
+        DetailService,
         {
             provide: 'IDetailRepository',
             useClass: DetailRepository,
